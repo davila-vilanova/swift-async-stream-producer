27e00e48b554aa1bf7fb919e51e2afd8df25485f
David Dávila <david@davi.la>
2023-07-05 20:37:36 -0500

    Add sources and package definition

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+  name: "AsyncStreamProducer",
+  platforms: [
+    .iOS(.v14),
+    .macOS(.v10_15),
+  ],
+  products: [
+    .library( // vend shareable code as a library product
+      name: "AsyncStreamProducer",
+      targets: ["AsyncStreamProducer"]),
+  ],
+  targets: [
+    .target( // compile sources to binary products
+      name: "AsyncStreamProducer",
+      dependencies: []),
+    .testTarget( // compile sources to test binary products
+      name: "AsyncStreamProducerTests",
+      dependencies: ["AsyncStreamProducer"]),
+  ]
+)
